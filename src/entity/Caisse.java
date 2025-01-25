9074d1f8175a1830a8ec67f633bb593d5b3bcd6b
yasssine19 <167819830+yasssine19@users.noreply.github.com>
2025-01-25 01:15:50 +0100

    Create Caisse.java

---
+++ src/entity/Caisse.java
@@ -0,0 +1,35 @@
+package entity;
+
+public class Caisse extends Mobile {
+	
+	public Caisse(Position position, Entrepot entrepot) {
+		super(position, entrepot);
+	}
+
+	public boolean deplacerCaisse(Direction direction) {
+		Position newPosition = deplacer(direction);
+		
+		ContenuZone contenu = entrepot.getContenu(newPosition);
+		if (contenu == null) return false;
+        
+        switch (contenu) {
+        	case ZONE_VIDE:
+	        	if (entrepot.getContenu(position) == ContenuZone.CAISSE_SUR_CIBLE) {
+	        		entrepot.setContenu(position, ContenuZone.CIBLE);
+	        	}
+	        	else entrepot.setContenu(position, ContenuZone.ZONE_VIDE);
+	        	entrepot.setContenu(newPosition, ContenuZone.CAISSE);
+	        	this.setPosition(newPosition);
+	        	return true;
+        
+    		case CIBLE:
+	        	entrepot.setContenu(position, ContenuZone.ZONE_VIDE);
+	        	entrepot.setContenu(newPosition, ContenuZone.CAISSE_SUR_CIBLE);
+	        	this.setPosition(newPosition);
+	            return true;
+	            
+            default:
+            	return false;
+        }
+	}
+}

42e88c047101a8d720d24b74abb182e7ad775aea
yasssine19 <167819830+yasssine19@users.noreply.github.com>
2025-01-25 01:17:19 +0100

    Create Direction.java

---
+++ src/entity/Direction.java
@@ -0,0 +1,15 @@
+package entity;
+
+/**
+ * Énumération des directions pour le jeu Sokoban 
+ * 
+ * @author Dominique Marcadet
+ * @version 1.2
+ *
+ */
+public enum Direction {
+    HAUT,
+    BAS,
+    GAUCHE,
+    DROITE;
+}

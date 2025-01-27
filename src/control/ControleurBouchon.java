13475a1e1eeed897081936481066bb5a87419b7b
yasssine19 <167819830+yasssine19@users.noreply.github.com>
2025-01-27 20:09:43 +0100

    Update ControleurBouchon.java

--- src/control/ControleurBouchon.java
+++ src/control/ControleurBouchon.java
@@ -3,16 +3,24 @@ package control;
 import entity.ContenuZone;
 import entity.Direction;
 import entity.Entrepot;
+import entity.Niveau;
 import entity.Position;
 
 public class ControleurBouchon implements IControleur {
 
 	private Entrepot entrepot;
+	private Niveau niveauActuel;
 	
-	public ControleurBouchon() {
+	public ControleurBouchon(Niveau niveau) {
 		try {
-			this.entrepot = new Entrepot();
+			this.entrepot = new Entrepot(niveau);
 		} catch (Exception e) {}
+		
+		niveauActuel = niveau;
+	}
+	
+	public Niveau getNiveauActuel() {
+		return entrepot.getNiveauActuel();
 	}
 	
 	@Override
@@ -24,7 +32,7 @@ public class ControleurBouchon implements IControleur {
 	public boolean jeuGagné() {
         for (int l = 0; l < getNbLignes(); l++) {
             for (int c = 0; c < getNbColonnes(); c++) {
-                if (entrepot.getContenu(new Position(l, c)) == ContenuZone.CAISSE) {
+                if (entrepot.getContenu(new Position(l, c)) == ContenuZone.CAISSE || entrepot.getContenu(new Position(l, c)) == ContenuZone.CIBLE) {
                     return false;
                 }
             }
@@ -37,12 +45,16 @@ public class ControleurBouchon implements IControleur {
 		for (int l = 0; l < getNbLignes(); l++) {
             for (int c = 0; c < getNbColonnes(); c++) {
                 if (entrepot.getContenu(new Position(l, c)) == ContenuZone.CAISSE) {
-                    int counter = 0;
-                    if (entrepot.getContenu(new Position(l-1, c)) == ContenuZone.MUR) counter++;
-                    if (entrepot.getContenu(new Position(l, c-1)) == ContenuZone.MUR) counter++;
-                    if (entrepot.getContenu(new Position(l+1, c)) == ContenuZone.MUR) counter++;
-                    if (entrepot.getContenu(new Position(l, c+1)) == ContenuZone.MUR) counter++;
-                    if (counter >= 2) return true;
+                    if (entrepot.getContenu(new Position(l-1, c)) == ContenuZone.MUR && entrepot.getContenu(new Position(l, c-1)) == ContenuZone.MUR) return true;
+                    if (entrepot.getContenu(new Position(l-1, c)) == ContenuZone.MUR && entrepot.getContenu(new Position(l, c+1)) == ContenuZone.MUR) return true;
+                    if (entrepot.getContenu(new Position(l+1, c)) == ContenuZone.MUR && entrepot.getContenu(new Position(l, c-1)) == ContenuZone.MUR) return true;
+                    if (entrepot.getContenu(new Position(l+1, c)) == ContenuZone.MUR && entrepot.getContenu(new Position(l, c+1)) == ContenuZone.MUR) return true;
+                    if (entrepot.getNiveauActuel() == Niveau.NIVEAU2) {
+                    	if (entrepot.getContenu(new Position(l+1, c)) == ContenuZone.MUR && entrepot.getContenu(new Position(l, c+2)) == ContenuZone.MUR &&
+                		entrepot.getContenu(new Position(l, c+1)) == ContenuZone.CAISSE_SUR_CIBLE) return true;
+                    	if (entrepot.getContenu(new Position(l+1, c)) == ContenuZone.CAISSE_SUR_CIBLE && entrepot.getContenu(new Position(l+2, c)) == ContenuZone.MUR &&
+                		entrepot.getContenu(new Position(l, c+1)) == ContenuZone.MUR) return true;
+                    }
                 }
             }
         }

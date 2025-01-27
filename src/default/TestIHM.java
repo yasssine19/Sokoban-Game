7f11bb1be414bb5f3f315dbc22c3c44582e95f9b
yasssine19 <167819830+yasssine19@users.noreply.github.com>
2025-01-27 20:09:28 +0100

    Update TestIHM.java

--- src/default/TestIHM.java
+++ src/default/TestIHM.java
@@ -2,19 +2,13 @@ import javax.swing.SwingUtilities;
 
 import boundary.FenetreSokoban;
 import control.ControleurBouchon;
+import entity.Niveau;
 
-/**
- * Application de test de l'IHM du jeu Sokoban en utilisant le controleur bouchon
- * 
- * @author Dominique Marcadet
- * @version 1.2
- *
- */
 public class TestIHM implements Runnable {
 
 	@Override
 	public void run() {
-    	new FenetreSokoban(new ControleurBouchon());
+    	new FenetreSokoban(new ControleurBouchon(Niveau.NIVEAU1));
 	}
 
     public static void main(String[] args) {
